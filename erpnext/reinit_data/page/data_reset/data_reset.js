51cc46ae6ef74abe83024a2639f05989d0f021da
zazart <antemazazart@gmail.com>
2025-04-30 20:56:35 +0300

    feat/static-view

--- erpnext/reinit_data/page/data_reset/data_reset.js
+++ erpnext/reinit_data/page/data_reset/data_reset.js
@@ -18,13 +18,13 @@ frappe.pages['data-reset'].on_page_load = function(wrapper) {
 		frappe.msgprint("Clicked Refresh");
 	});
 
-	page.add_menu_item('Send Email',() => {
-		frappe.msgprint("Clicked Send Email");
-	});
+	// page.add_menu_item('Menu item ',() => {
+	// 	frappe.msgprint("Clicked Menu Item");
+	// });
     
-	page.add_action_item('Delete',() => {
-		frappe.msgprint("Clicked Delete");
-	});
+	// page.add_action_item('Delete',() => {
+	// 	frappe.msgprint("Clicked Delete");
+	// });
 
 	// Field ID
 	let id_field = page.add_field({
@@ -66,51 +66,5 @@ frappe.pages['data-reset'].on_page_load = function(wrapper) {
 		}
 	});
 
-	
-	// // Ajoute plusieurs champs
-	// let status_field = page.add_field({
-	// 	label: 'Statut',
-	// 	fieldtype: 'Select',
-	// 	fieldname: 'status',
-	// 	options: ['Open', 'Closed', 'Canceled'],
-	// 	change() {
-	// 		frappe.msgprint('Statut : ' + status_field.get_value());
-	// 	}
-	// });
-
-
-	// // Bouton de filtrage
-	// let filter_btn = $('<button class="btn btn-primary ml-2">Filtrer</button>');
-	// page.page_form.append(filter_btn);
-
-	// filter_btn.on('click', () => {
-	// 	const statut = status_field.get_value();
-	// 	const utilisateur = utilisateur_field.get_value();
-
-	// 	frappe.msgprint(`Filtrage avec : ${utilisateur}, ${statut}`);
-	// });
-
-	// $(frappe.render_template("data_reset",{})).appendTo(page.body);
-	// $(frappe.render_template("data_reset", {
-	// 	data: "Hi Frappe"
-	// })).appendTo(page.body);
-	
-
-	// // Ajout des vues
-    // page.add_view("users", `
-	// 	<div style='padding:20px;'>Liste des utilisateurs</div>
-	// `);
-    // page.set_view("users");
-
-    // // Manipuler layout-side-section : Ajouter du contenu dans la sidebar
-    // const sideSection = page.wrapper.find(".layout-side-section");
-    
-    // // Ajouter un titre et un bouton dans la sidebar
-    // sideSection.append(`
-    //     <div class="p-3">
-    //         <h5>Actions rapides</h5>
-    //         <button class="btn btn-sm btn-secondary mb-2" onclick="frappe.msgprint('Import lancé')">Importer données</button>
-    //         <button class="btn btn-sm btn-danger" onclick="frappe.msgprint('Données réinitialisées')">Réinitialiser</button>
-    //     </div>
-    // `);
-};
+	$(frappe.render_template("data_reset",{})).appendTo(page.body);
+}
